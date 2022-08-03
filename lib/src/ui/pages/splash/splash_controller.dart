d72000dbd84a68393f9314f6373a554ec0bdf16b
Alex Astudillo <astudilloalex1@gmail.com>
2022-08-03 17:43:05 -0500

    Complete splash screen

--- lib/src/ui/pages/splash/splash_controller.dart
+++ lib/src/ui/pages/splash/splash_controller.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 import 'package:just_jewelry/src/app/app.dart';
+import 'package:just_jewelry/src/ui/routes/route_names.dart';
 
 class SplashController extends GetxController {
   SplashController({
@@ -7,4 +8,20 @@ class SplashController extends GetxController {
   });
 
   final AuthController authController;
+
+  final RxBool _loading = RxBool(true);
+
+  Future<String> get route async {
+    try {
+      await authController.loadCurrentUser();
+      if (authController.userDetail == null) return RouteNames.signIn;
+      return RouteNames.home;
+    } on Exception {
+      return RouteNames.signIn;
+    } finally {
+      _loading(false);
+    }
+  }
+
+  bool get loading => _loading.value;
 }
--- lib/src/ui/pages/splash/splash_page.dart
+++ lib/src/ui/pages/splash/splash_page.dart
@@ -1,10 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:just_jewelry/src/ui/pages/splash/splash_controller.dart';
+import 'package:just_jewelry/src/ui/widgets/progress_indicators/custom_progress_indicator.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Image.asset(
+                'assets/images/diamond_gold.png',
+                width: 200.0,
+                color: Colors.white,
+              ),
+            ),
+            Text(
+              'justJewelry'.tr,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                fontSize: 40.0,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 10.0),
+            GetX<SplashController>(
+              didChangeDependencies: (state) async {
+                Get.offNamed(await state.controller!.route);
+              },
+              builder: (controller) {
+                if (controller.loading) {
+                  return const CustomProgressIndicator(
+                    color: Colors.white,
+                    animationColor: Color(0xFFAEAF7A),
+                    showText: false,
+                  );
+                }
+                return const SizedBox.shrink();
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
